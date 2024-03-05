f316c6f113f204c5ac49cc7db741fe748f504345
Annette <159702924+annga864@users.noreply.github.com>
2024-03-05 19:17:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function beraknaResultat() {
         //allaSvar[i - 1] = document.getElementsByName('fraga' + i);
         let nameId = "fraga" + i;
         let selectedId = document.getElementsByName(nameId);
-        for (let j = 0; j < selectedId.length; j++){
-            if (selectedId[j].checked){
+        for (let j = 0; j < selectedId.length; j++) {
+            if (selectedId[j].checked) {
                 console.log(selectedId[j]);
                 allaSvar[i - 1] = selectedId[j].value;
             }
@@ -24,46 +24,52 @@ function beraknaResultat() {
 
     for (let i = 0; i < allaSvar.length; i++) {
 
-        if (allaSvar[i] == 'gul'){
+        if (allaSvar[i] == 'gul') {
             gulPoang++;
         }
-        else if(allaSvar[i] == 'gron'){
+        else if (allaSvar[i] == 'gron') {
             gronPoang++;
         }
-        else if (allaSvar[i] == 'rosa'){
+        else if (allaSvar[i] == 'rosa') {
             rosaPoang++;
         }
     }
-    
+
     console.log(gulPoang); console.log(rosaPoang); console.log(gronPoang);
-    if(document.getElementById('knapp2') != null){
-        document.getElementById('knapp2').remove();
-    }
     const resultatDiv = document.createElement('div');
     const resultatP = document.createElement('p1');
     resultatDiv.id = "resultat";
     let resultatKar = '';
-
-
-    if (gulPoang > rosaPoang && gulPoang > gronPoang) {
-        resultatKar = 'Du är Jonas!';
+    if (rosaPoang == 0) {
+    } else if (gronPoang == 0) {
+    } else if (gulPoang == 0) {
+    } else {
+       
+        if (gulPoang > rosaPoang && gulPoang > gronPoang) {
+            resultatKar = 'Du är Jonas!';
+        }
+        else if (rosaPoang > gulPoang && rosaPoang > gronPoang) {
+            resultatKar = 'Du är Lisa!';
+        } else if (gronPoang > gulPoang && gronPoang > rosaPoang) {
+            resultatKar = 'Du är Sara!';
+        } else if (gronPoang == gulPoang) {
+            resultatKar = 'Du är en blandning av Sara & Jonas!';
+        } else if (rosaPoang == gulPoang) {
+            resultatKar = 'Du är en blandning av Lisa & Jonas!';
+        } else if (gronPoang == rosaPoang) {
+            resultatKar = 'Du är en blandning av Sara & Lisa!';
+        } else if (gronPoang == gulPoang && gronPoang == rosaPoang && gulPoang == rosaPoang) {
+            resultatKar = 'Du är en blandning av alla tre!';
+        } const resultatText = document.createTextNode(resultatKar);
+        resultatP.appendChild(resultatText);
+        resultatDiv.appendChild(resultatP);/*
+        document.getElementById("knapp2").appendChild(resultatDiv);*/
+        if (document.getElementById('knapp2') != null) {
+            console.log('test');
+            const para = document.getElementById("knapp15");
+                document.getElementById('knapp2').replaceChild(resultatDiv, para);
+            }
     }
-     else if (rosaPoang > gulPoang && rosaPoang > gronPoang) {
-        resultatKar = 'Du är Lisa!';
-    } else if (gronPoang > gulPoang && gronPoang > rosaPoang) {
-        resultatKar = 'Du är Sara!';
-    } else if (gronPoang == gulPoang) {
-        resultatKar = 'Du är en blandning av Sara & Jonas!';
-    } else if (rosaPoang == gulPoang) {
-        resultatKar = 'Du är en blandning av Lisa & Jonas!';
-    } else if (gronPoang == rosaPoang) {
-        resultatKar = 'Du är en blandning av Sara & Lisa!';
-    } else if (gronPoang == gulPoang && gronPoang == rosaPoang && gulPoang == rosaPoang) {
-        resultatKar = 'Du är en blandning av alla tre!';
-    }
 
-    const resultatText = document.createTextNode(resultatKar);
-    resultatP.appendChild(resultatText);
-    resultatDiv.appendChild(resultatP);
-    document.body.appendChild(resultatDiv);
+
 }
